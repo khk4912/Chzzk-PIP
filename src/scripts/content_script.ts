3281a981bae29b19ea62aab0f7776cd9d6913e23
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-12-22 17:35:41 +0900

    ⚰️ chore: Remove unused imports

--- src/scripts/content_script.ts
+++ src/scripts/content_script.ts
@@ -1,5 +1,4 @@
 import { addButton, waitForElement } from './utils/inject_btns'
-import { injectOverlay, updateOverlay } from './utils/overlay'
 
 async function main (): Promise<void> {
   const btn = await waitForElement('.pzp-pc__bottom-buttons-right')
