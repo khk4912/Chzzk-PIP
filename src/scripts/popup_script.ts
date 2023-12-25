537f241a3d9f8b979fc3e55c1cae7e0678d527d9
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-12-25 20:24:29 +0900

    ✨ feat: Implement on / off in popup page

---
+++ src/scripts/popup_script.ts
@@ -0,0 +1,66 @@
+import type { Option } from './types/option'
+
+const [pipSwitch, recSwitch, fastSaveSwitch] =
+  document.querySelectorAll('input[type="checkbox"]')
+
+function init (): void {
+  if (!(pipSwitch instanceof HTMLInputElement &&
+    recSwitch instanceof HTMLInputElement &&
+    fastSaveSwitch instanceof HTMLInputElement)) {
+    return
+  }
+
+  void (async () => {
+    const option: Option = (await chrome.storage.local.get('option'))?.option ?? {}
+
+    if (option === undefined) {
+      const newOption: Option = {
+        pip: true,
+        rec: true,
+        fastRec: false
+      }
+      await chrome.storage.local.set({ option: newOption })
+    }
+
+    pipSwitch.checked = option?.pip ?? true
+    recSwitch.checked = option?.rec ?? true
+    fastSaveSwitch.checked = option?.fastRec ?? false
+  })()
+}
+
+async function handleChange (e: Event): Promise<void> {
+  if (!(e.target instanceof HTMLInputElement)) {
+    return
+  }
+
+  if (!(pipSwitch instanceof HTMLInputElement &&
+    recSwitch instanceof HTMLInputElement &&
+    fastSaveSwitch instanceof HTMLInputElement)) {
+    return
+  }
+
+  const option: Option = (await chrome.storage.local.get('option'))?.option ?? {}
+
+  switch (e.target) {
+    case pipSwitch:
+      option.pip = e.target.checked
+      await chrome.storage.local.set({ option })
+      break
+    case recSwitch:
+      console.log('recSwitch')
+      option.rec = e.target.checked
+      await chrome.storage.local.set({ option })
+      break
+    case fastSaveSwitch:
+      option.fastRec = e.target.checked
+      await chrome.storage.local.set({ option })
+      break
+  }
+}
+
+init()
+document.addEventListener('change', (e) => {
+  void (async () => {
+    await handleChange(e)
+  })()
+})
---
+++ src/scripts/types/option.ts
@@ -0,0 +1,5 @@
+export interface Option {
+  pip?: boolean
+  rec?: boolean
+  fastRec?: boolean
+}
