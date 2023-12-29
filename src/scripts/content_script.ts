8dae1664d0eb25a16460c785819dd0d772e0349b
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-12-29 17:11:48 +0800

    ✨ feat: Register seek event listener

--- src/scripts/content_script.ts
+++ src/scripts/content_script.ts
@@ -1,10 +1,12 @@
 import { addButton, recordShortcut, waitForElement } from './utils/inject/inject_btns'
+import { registerSeekHandler } from './utils/seek/seek'
 
 async function main (): Promise<void> {
   const btn = await waitForElement('.pzp-pc__bottom-buttons-right')
   addButton(btn as HTMLDivElement)
 
   document.addEventListener('keydown', recordShortcut)
+  registerSeekHandler()
 }
 
 void main()
