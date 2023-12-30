a93af4e332436c8377edad9f612b8cdd41b451d1
khk4912 <30457148+khk4912@users.noreply.github.com>
2023-12-31 00:50:04 +0900

    ♻️ refactor: Refactor for supporting multiple transcode

--- src/scripts/utils/record/transcode.ts
+++ src/scripts/utils/record/transcode.ts
@@ -4,6 +4,7 @@ import type { SupportedType } from '../../types/record'
 const { createFFmpeg, fetchFile } = FFmpeg
 
 const ffmpeg = createFFmpeg({
+  log: true,
   mainName: 'main',
   corePath: chrome.runtime.getURL('ffmpeg/ffmpeg-core.js')
 })
@@ -18,16 +19,48 @@ export const transcode = async (
 
   await ffmpeg.load()
 
-  const outputFileName = `output.${outputType}`
-  const blobType = outputType === 'gif'
-    ? 'image/gif'
-    : 'video/{outputType}'
+  switch (outputType) {
+    case 'gif':
+      return await transcodeGIF(inputFile)
+    case 'webp':
+      return await transcodeWebP(inputFile)
+    default:
+      return await transcodeMP4(inputFile)
+  }
+}
 
-  ffmpeg.FS('writeFile', 'input.webm', await fetchFile(inputFile))
-  await ffmpeg.run('-i', 'input.webm', '-c', 'copy', outputFileName)
+const transcodeGIF = async (inputFileURL: string): Promise<string> => {
+  const inputFile = await fetchFile(inputFileURL)
 
-  const data = ffmpeg.FS('readFile', outputFileName)
-  const url = URL.createObjectURL(new Blob([data.buffer], { type: blobType }))
+  ffmpeg.FS('writeFile', 'input.webm', inputFile)
+  await ffmpeg.run('-i', 'input.webm', '-ss', '30', '-t', '3', '-vf', 'fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse"', '-c', 'copy', 'output.gif')
+
+  const data = ffmpeg.FS('readFile', 'output.gif')
+  const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }))
+
+  return url
+}
+
+const transcodeWebP = async (inputFileURL: string): Promise<string> => {
+  const inputFile = await fetchFile(inputFileURL)
+
+  ffmpeg.FS('writeFile', 'input.webm', inputFile)
+  const vFilter = 'fps=' + '10' + ',scale=iw/100*' + '50' + ':-1:flags=lanczos'
+  await ffmpeg.run('-i', 'input.webm', '-vf', vFilter, '-vcodec', 'libwebp', '-lossless', '0', '-compression_level', '6', '-q:v', '50', '-loop', '0', '-preset', 'picture', '-an', '-vsync', '0', 'output.webp')
+  const data = ffmpeg.FS('readFile', 'output.webp')
+  const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/webp' }))
+
+  return url
+}
+
+const transcodeMP4 = async (inputFileURL: string): Promise<string> => {
+  const inputFile = await fetchFile(inputFileURL)
+
+  ffmpeg.FS('writeFile', 'input.webm', inputFile)
+  await ffmpeg.run('-i', 'input.webm', '-c', 'copy', 'output.mp4')
+
+  const data = ffmpeg.FS('readFile', 'output.mp4')
+  const url = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }))
 
   return url
 }
