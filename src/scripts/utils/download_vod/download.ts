5b932eb798469251e6a51da69e38a6f449644873
khk4912 <30457148+khk4912@users.noreply.github.com>
2024-01-05 16:28:13 +0900

    ✨ feat: Implement download API client

---
+++ src/scripts/utils/download_vod/download.ts
@@ -0,0 +1,61 @@
+import type { PlayBackURL, Video, VideoInfo } from '../../types/vod'
+
+export function isVODPage (): boolean {
+  return location.pathname.startsWith('/video')
+}
+
+export async function openDownloadVODPage (): Promise<void> {
+  const videoNumber = Number(location.pathname.split('/')[2])
+
+  const videoInfo = await getVideoInfo(videoNumber)
+  const playBackURL = await getPlayBackURL(videoInfo.videoID, videoInfo.inKey)
+
+  await chrome.storage.local.set({ playBackURL })
+  window.open(chrome.runtime.getURL('pages/download_vod.html'))
+}
+
+async function getVideoInfo (videoNumber: number): Promise<VideoInfo> {
+  const r = await fetch(`https://api.chzzk.naver.com/service/v1/videos/${videoNumber}`)
+  const data: Video = await r.json()
+
+  const videoTitle = data.content.videoTitle
+  const inKey = data.content.inKey
+  const videoID = data.content.videoId
+
+  return { videoTitle, inKey, videoID }
+}
+
+async function getPlayBackURL (videoID: string, inKey: string): Promise<PlayBackURL[]> {
+  const r = await fetch(`https://apis.naver.com/neonplayer/vodplay/v1/playback/${videoID}?key=${inKey}&sid=2099&env=real&lc=ko&cpl=ko`,
+    {
+      headers: {
+        Accept: 'application/xml'
+      }
+    }
+  )
+
+  const data = new DOMParser().parseFromString(await r.text(), 'text/xml')
+  const mp4AdaptionSet = data.querySelectorAll('AdaptationSet[mimeType="video/mp4"] > Representation')
+
+  const res: PlayBackURL[] = []
+
+  mp4AdaptionSet.forEach((rep) => {
+    const fpsKind = rep.querySelector('[kind="fps"]')?.innerHTML
+    const resolutionKind = rep.querySelector('[kind="resolution"]')?.innerHTML
+    const baseURL = rep.querySelector('BaseURL')?.innerHTML
+
+    if (fpsKind === undefined ||
+        resolutionKind === undefined ||
+        baseURL === undefined) {
+      return
+    }
+
+    res.push({
+      resolution: parseInt(resolutionKind),
+      fps: parseInt(fpsKind),
+      url: baseURL
+    })
+  })
+
+  return res
+}
