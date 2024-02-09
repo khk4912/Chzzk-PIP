816ccfa482f3459298009084da16e6e9763b5a39
khk4912 <30457148+khk4912@users.noreply.github.com>
2024-02-09 22:21:36 +0900

    ✨ feat: Monkeypatch HLS option

--- src/scripts/monkeypatch_core.ts
+++ src/scripts/monkeypatch_core.ts
@@ -1,4 +1,10 @@
+import { getCorePlayer } from './utils/seek/react_helper'
+
+// TODO: https://github.com/video-dev/hls.js/blob/master/docs/API.md 보고 연구해보기
 
 (() => {
+  const player = getCorePlayer()
+  const hls = player.player._mediaController._hls
 
+  hls.config.backBufferLength = Infinity // 브라우저에게 관리 맡기기
 })()
