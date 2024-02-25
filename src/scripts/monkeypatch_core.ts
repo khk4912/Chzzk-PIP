314bc18f057d4fbdfbe541e1299635ed52d29184
khk4912 <30457148+khk4912@users.noreply.github.com>
2024-02-25 12:34:12 +0900

    🐛 fix: Check _hls is not undefined

--- src/scripts/monkeypatch_core.ts
+++ src/scripts/monkeypatch_core.ts
@@ -9,7 +9,12 @@ import { getCorePlayer } from './utils/seek/react_helper'
     return
   }
 
-  const hls = player.player._mediaController._hls
+  const hls = player.player._mediaController?._hls
+
+  if (hls === undefined ||
+      hls === null) {
+    return
+  }
 
   hls.config.backBufferLength = Infinity // 브라우저에게 관리 맡기기
   hls.config.maxMaxBufferLength = 1200
