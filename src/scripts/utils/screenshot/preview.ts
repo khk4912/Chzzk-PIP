00f76d28e280cae2f913c52cf907345b3b748257
khk4912 <30457148+khk4912@users.noreply.github.com>
2024-02-25 14:13:02 +0900

    ✨ feat: Overlay

--- src/scripts/utils/screenshot/preview.ts
+++ src/scripts/utils/screenshot/preview.ts
@@ -3,30 +3,35 @@ export function createDraggablePreview (imageDataURL: string, video: HTMLVideoEl
 
   const preview = document.createElement('img')
   preview.classList.add('chzzk-pip', 'screenshot-preview')
-  preview.src = imageDataURL
 
+  preview.src = imageDataURL
   preview.style.cssText = `
+    position: absolute;
+    width: 100%;
+    height: auto;
+    `
+
+  const wrappedOverlay = wrapWithOverlay(preview)
+  wrappedOverlay.style.cssText = `
     position: absolute;
     left: ${rect.left * 1.2}px;
     top: ${rect.top * 1.2}px;
-    resize: both;
     cursor: move;
     width: ${rect.width * 0.8}px;
     height: auto;
     `
-
-  document.body.appendChild(preview)
+  document.body.appendChild(wrappedOverlay)
 
   let startPosX = 0
   let startPosY = 0
 
-  preview.addEventListener('mousedown', (e) => {
+  wrappedOverlay.addEventListener('mousedown', (e) => {
     const mouseMove = (e: MouseEvent): void => {
       const newPosX = startPosX - e.clientX
       const newPosY = startPosY - e.clientY
 
-      preview.style.left = `${preview.offsetLeft - newPosX}px`
-      preview.style.top = `${preview.offsetTop - newPosY}px`
+      wrappedOverlay.style.left = `${wrappedOverlay.offsetLeft - newPosX}px`
+      wrappedOverlay.style.top = `${wrappedOverlay.offsetTop - newPosY}px`
 
       startPosX = e.clientX
       startPosY = e.clientY
@@ -43,4 +48,24 @@ export function createDraggablePreview (imageDataURL: string, video: HTMLVideoEl
       document.removeEventListener('mousemove', mouseMove)
     })
   })
+}
+
+function wrapWithOverlay (img: HTMLImageElement): HTMLDivElement {
+  const overlay = document.createElement('div')
+  const cover = document.createElement('div')
+
+  cover.innerHTML = 'Drag to move'
+
+  cover.style.cssText = `
+  position: absolute;
+  bottom: 0;
+  `
+
+  overlay.classList.add('chzzk-screenshot-overlay')
+  cover.classList.add('chzzk-screenshot-cover')
+
+  overlay.appendChild(img)
+  overlay.appendChild(cover)
+
+  return overlay
 }
