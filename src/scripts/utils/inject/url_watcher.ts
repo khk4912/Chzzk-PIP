a49b4542bfa225d74fbc9df21b3c6c0bd749807d
khk4912 <30457148+khk4912@users.noreply.github.com>
2024-04-05 20:40:03 +0900

    ⚡️ feat: Use DOM Navigation API instead of MutationObserver

--- src/scripts/utils/inject/url_watcher.ts
+++ src/scripts/utils/inject/url_watcher.ts
@@ -2,31 +2,30 @@ import { waitForElement, addButton } from './inject_btns'
 import { registerSeekHandler } from '../seek/seek'
 
 async function main (): Promise<void> {
-  await waitForElement('.pzp-pc__bottom-buttons-right')
+  await waitForElement('.pzp-pc__bottom-buttons-right').catch()
 
   addButton()
   void registerSeekHandler()
 }
 
 export const injectButton = (): void => {
-  let oldHref = window.location.href
-  const body = document.querySelector('body') as HTMLElement
-
+  // const oldHref = window.location.href
+  // const body = document.querySelector('body') as HTMLE
   const inject = (): void => {
     void main()
   }
 
   inject()
+  window.navigation.addEventListener('navigate', inject)
+  // const _observer = new MutationObserver(() => {
+  //   if (oldHref !== window.location.href) {
+  //     oldHref = window.location.href
+  //     inject()
+  //   }
+  // })
 
-  const _observer = new MutationObserver(() => {
-    if (oldHref !== window.location.href) {
-      oldHref = window.location.href
-      inject()
-    }
-  })
-
-  _observer.observe(body, {
-    childList: true,
-    subtree: true
-  })
+  // _observer.observe(body, {
+  //   childList: true,
+  //   subtree: true
+  // })
 }
