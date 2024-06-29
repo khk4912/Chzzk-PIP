e2d29631a8d1904b4e5dba83b63e9de78157637b
khk4912 <30457148+khk4912@users.noreply.github.com>
2024-06-29 19:25:55 +0900

    🔊 chore: Add downloadID logging for debugging

--- src/scripts/download_clip.ts
+++ src/scripts/download_clip.ts
@@ -1,9 +1,11 @@
 void (async () => {
   const { clipDownloadURL, title } = await chrome.storage.local.get(['clipDownloadURL', 'title']) as { clipDownloadURL: string, title: string }
+
   chrome.downloads.download({
     url: clipDownloadURL,
     filename: `${title.replace(/[/\\?%*:|"<>]/g, '_')}.mp4`
-  }, () => {
+  }, (downloadID) => {
+    console.log(`Download started with ID: ${downloadID}`)
     window.close()
   })
 })()
