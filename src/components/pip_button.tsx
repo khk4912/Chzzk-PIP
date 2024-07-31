c6b3723652df50505f111259e151a41c7c41a868
khk4912 <30457148+khk4912@users.noreply.github.com>
2024-07-31 22:57:19 +0900

    ✨ feat: Add shortcut (P) in the PIP button

--- src/components/pip_button.tsx
+++ src/components/pip_button.tsx
@@ -1,12 +1,16 @@
 import PIPIcon from '../../static/pip.svg?react'
+import { useShortcut } from '../utils/hooks'
 
 export function PIPButton (): React.ReactNode {
+  const clickHandler = (): void => { void makeVideoPIP() }
+  useShortcut(['p', 'P', 'ㅔ'], clickHandler)
+
   return (
     <button
-      onClick={() => { void makeVideoPIP() }}
+      onClick={clickHandler}
       className='pzp-button pzp-pc-setting-button pzp-pc__setting-button pzp-pc-ui-button'
     >
-      <span className='pzp-pc-ui-button__tooltip pzp-pc-ui-button__tooltip--top'>PIP</span>
+      <span className='pzp-pc-ui-button__tooltip pzp-pc-ui-button__tooltip--top'>PIP (p)</span>
       <span className='pzp-ui-icon pzp-pc-setting-button__icon'>
         <PIPIcon />
       </span>
@@ -21,10 +25,14 @@ async function makeVideoPIP (): Promise<void> {
     return
   }
 
-  video.disablePictureInPicture = false
-  if (document.pictureInPictureElement != null) {
-    await document.exitPictureInPicture()
-  } else {
-    await video.requestPictureInPicture()
+  try {
+    video.disablePictureInPicture = false
+    if (document.pictureInPictureElement != null) {
+      await document.exitPictureInPicture()
+    } else {
+      await video.requestPictureInPicture()
+    }
+  } catch {
+    // Metadata 로드 안될 경우 오류 발생하므로 오류 무시
   }
 }
