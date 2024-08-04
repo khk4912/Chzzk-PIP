9da8d27118afff32de6099f97171727ddd61c9e9
khk4912 <30457148+khk4912@users.noreply.github.com>
2024-08-04 21:01:44 +0900

    ✨ feat: Implements transcode

--- pages/record_result/components/DownloadButtons.tsx
+++ pages/record_result/components/DownloadButtons.tsx
@@ -1,30 +1,59 @@
+import { useEffect } from 'react'
+import { useFFmpeg, toMP4, toMP4AAC, toGIF, toWEBP } from '../../../src/utils/record/transcode'
 import type { DownloadInfo } from '../../../types/record_info'
 import { ButtonBase } from './Button'
 
 export function DownloadButtons ({ downloadInfo }: { downloadInfo: DownloadInfo | undefined }): React.ReactNode {
-  const download = (): void => {
+  const download = (url: string | undefined, ext: string): void => {
+    if (url === undefined) {
+      return
+    }
+
     const a = document.createElement('a')
     a.href = downloadInfo?.recordInfo.resultBlobURL ?? ''
-    a.download = `${downloadInfo?.fileName ?? 'title'}.webm` ?? ''
+    a.download = `${downloadInfo?.fileName ?? 'title'}.${ext}` ?? ''
     a.click()
   }
 
+  const [ffmpeg, isFFmpegReady] = useFFmpeg()
+  useEffect(() => { console.log(isFFmpegReady) }, [isFFmpegReady])
+
   return (
     <div className={`download-buttons ${downloadInfo === undefined ? 'disabled' : ''}`}>
-      <ButtonBase onClick={download}>
+      <ButtonBase onClick={() => { download(downloadInfo?.recordInfo?.resultBlobURL, 'webm') }}>
         다운로드
       </ButtonBase>
 
       <h3>변환 후 다운로드</h3>
-      <div className='after-transcode'>
-        <ButtonBase onClick={download}>MP4</ButtonBase>
-        <ButtonBase onClick={download}>MP4 (호환성)</ButtonBase>
-        <ButtonBase onClick={download}>GIF</ButtonBase>
-        <ButtonBase onClick={download}>WEBP</ButtonBase>
+      <div className={`after-transcode ${!isFFmpegReady ? 'disabled' : ''}`}>
+        <ButtonBase onClick={() => {
+          void toMP4(ffmpeg.current, downloadInfo?.recordInfo.resultBlobURL ?? '')
+            .then((url) => { download(url, 'mp4') })
+        }}
+        >MP4
+        </ButtonBase>
+        <ButtonBase onClick={() => {
+          void toMP4AAC(ffmpeg.current, downloadInfo?.recordInfo.resultBlobURL ?? '')
+            .then((url) => { download(url, 'mp4') })
+        }}
+        >MP4 (호환성)
+        </ButtonBase>
+        <ButtonBase onClick={() => {
+          void toGIF(ffmpeg.current, downloadInfo?.recordInfo.resultBlobURL ?? '')
+            .then((url) => { download(url, 'gif') })
+        }}
+        >GIF
+        </ButtonBase>
+        <ButtonBase onClick={() => {
+          void toWEBP(ffmpeg.current, downloadInfo?.recordInfo.resultBlobURL ?? '')
+            .then((url) => { download(url, 'mp4') })
+        }}
+        >WEBP
+        </ButtonBase>
       </div>
       <div className='after-transcode'>
-        <ButtonBase onClick={download}>자르고 다운로드</ButtonBase>
-        <ButtonBase onClick={download}>분할 다운로드</ButtonBase>
+        <ButtonBase>자르고 다운로드</ButtonBase>
+        <ButtonBase>분할 다운로드</ButtonBase>
       </div>
 
     </div>
---
+++ src/utils/record/transcode.ts
@@ -0,0 +1,60 @@
+import { FFmpeg } from '@ffmpeg/ffmpeg'
+import { fetchFile } from '@ffmpeg/util'
+import { useEffect, useRef, useState } from 'react'
+
+const FFMPEG_OPTION = {
+  coreURL: chrome.runtime.getURL('ffmpeg/ffmpeg-core.js'),
+  wasmURL: chrome.runtime.getURL('ffmpeg/ffmpeg-core.wasm'),
+  workerURL: chrome.runtime.getURL('ffmpeg/ffmpeg-core.worker.js')
+}
+
+export function useFFmpeg (): [React.MutableRefObject<FFmpeg>, boolean] {
+  const ffmpeg = useRef<FFmpeg>(new FFmpeg())
+
+  ffmpeg.current.on('log', ({ message }) => { console.log(message) })
+
+  const [isReady, setIsReady] = useState(false)
+  useEffect(() => {
+    ffmpeg.current.load({ ...FFMPEG_OPTION })
+      .then(
+        () => {
+          console.log('Loaded!')
+          setIsReady(true)
+        }
+      )
+      .catch(console.log)
+  }, [])
+
+  return [ffmpeg, isReady] as const
+}
+
+export async function toMP4 (ffmpeg: FFmpeg, inputFileURL: string): Promise<string> {
+  return await _transcode(ffmpeg, inputFileURL, 'output.mp4', ['-c', 'copy'])
+}
+
+export async function toMP4AAC (ffmpeg: FFmpeg, inputFileURL: string): Promise<string> {
+  return await _transcode(ffmpeg, inputFileURL, 'output.mp4', ['-c:a', 'aac'])
+}
+
+export async function toGIF (ffmpeg: FFmpeg, inputFileURL: string): Promise<string> {
+  return await _transcode(ffmpeg, inputFileURL, 'output.gif', ['-vf', 'fps=30,scale=320:-1'])
+}
+
+export async function toWEBP (ffmpeg: FFmpeg, inputFileURL: string): Promise<string> {
+  return await _transcode(ffmpeg, inputFileURL, 'output.webp', ['-c', 'copy'])
+}
+
+async function _transcode (
+  ffmpeg: FFmpeg,
+  inputFileURL: string,
+  outputFileName: string,
+  args: string[],
+  type?: string): Promise<string> {
+  const input = await fetchFile(inputFileURL)
+  await ffmpeg.writeFile('input.webm', input)
+
+  await ffmpeg.exec(['-i', 'input.webm', ...args, outputFileName])
+  const output = await ffmpeg.readFile(outputFileName)
+
+  return URL.createObjectURL(new Blob([output], { type: type === undefined ? 'video/mp4' : `video/${type}` }))
+}
