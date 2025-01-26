1d1a0ef0a2ad12ad7ec34cd4c5f6af85f9c7743e
khk4912 <kosame@kosame.dev>
2025-01-26 16:21:22 +0900

    ⏪️ revert: Revert "🔧 feat: Add automated zip-file generation process"
    This reverts commit aa0c3b8434ec47e1579a085658a39eef6dcd1296.

--- vite.config.firefox.ts
+++ vite.config.firefox.ts
@@ -4,28 +4,19 @@ import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import svgr from 'vite-plugin-svgr'
-import { rmDotVite } from './custom-plugins'
 
 import manifest from './manifest.firefox.json'
-import zipPack from 'vite-plugin-zip-pack'
 
 // not dist, dist-firefox
 export default defineConfig({
-  plugins: [react(), crx({ manifest, browser: 'firefox' }), svgr(), rmDotVite(), zipPack(
-    {
-      outDir: 'packages',
-      inDir: 'dist-firefox',
-      outFileName: `${manifest.name.replaceAll(' ', '-')}-v${manifest.version}-Firefox.zip`,
-    }
-
-  )],
+  plugins: [react(), crx({ manifest, browser: 'firefox' }), svgr()],
   root: resolve(__dirname, ''),
   build: {
     outDir: resolve(__dirname, 'dist-firefox'),
     rollupOptions: {
       input: {
         record: resolve(__dirname, 'pages/record_result/index.html'),
-      },
+          }, 
     }
   },
   css: {
--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,11 @@ import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import svgr from 'vite-plugin-svgr'
-import zipPack from 'vite-plugin-zip-pack'
 
 import manifest from './manifest.json'
 
 export default defineConfig({
-  plugins: [react(), crx({ manifest }), svgr(), zipPack(
-    {
-      outDir: 'packages',
-      inDir: 'dist',
-      outFileName: `${manifest.name.replaceAll(' ', '-')}-v${manifest.version}.zip`,
-    }
-  )],
+  plugins: [react(), crx({ manifest }), svgr()],
   root: resolve(__dirname, ''),
   build: {
     rollupOptions: {
