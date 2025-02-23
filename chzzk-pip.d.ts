dbecc8bc10c0029eed33573e2c9d7259a2b7ff38
khk4912 <30457148+khk4912@users.noreply.github.com>
2025-02-23 19:02:41 +0900

    🙈 chore: Update .gitignore to add chzzk-pip.d.ts

---
+++ chzzk-pip.d.ts
@@ -0,0 +1,34 @@
+interface HTMLVideoElement {
+  captureStream: () => MediaStream
+  mozCaptureStream: () => MediaStream
+}
+
+interface RecordInfoBase {
+  startDateTime: number // new Date().getTime()
+  stopDateTime: number
+  resultBlobURL: string
+  streamInfo: StreamInfo
+  isMP4: boolean
+  highFrameRec?: boolean
+  audioBlobURL?: string
+}
+
+interface HighFPSRecordInfo extends RecordInfoBase {
+  highFrameRec?: true
+  audioBlobURL: string
+}
+
+interface LowFPSRecordInfo extends RecordInfoBase {
+  highFrameRec?: false // Set the type to 'false' for highFrameRec
+  audioBlobURL?: undefined // Set the type to 'undefined' for audioBlobURL
+}
+
+type RecordInfo = HighFPSRecordInfo | LowFPSRecordInfo
+
+interface MediaRecorder {
+  tempBlobURL?: string
+  recordInfo?: RecordInfo
+  stopFlag?: boolean
+  blobSize?: number
+  videoBlob?: Blob[]
+}
