88a49b66fc5fff59f11d6c35804a7f3ccca7606d
khk4912 <30457148+khk4912@users.noreply.github.com>
2025-02-26 22:38:45 +0900

    🚑 fix: Fix an issue where MaxHQ is not working

--- src/utils/max_hq.ts
+++ src/utils/max_hq.ts
@@ -1,6 +1,7 @@
 import { waitForElement } from '../inject_btn'
 
 export async function setMaxHQ (): Promise<void> {
-  const x = await waitForElement('.pzp-pc-setting-quality-pane__list-container > li') as HTMLLIElement
+  const x = await waitForElement('.pzp-setting-quality-pane__list-container > ul > li') as HTMLLIElement
+  x.replaceWith(x.cloneNode(true))
   x.click()
 }
