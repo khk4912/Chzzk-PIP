1baaa616fbe3db5faafbfbb931892c557e1c5779
khk4912 <30457148+khk4912@users.noreply.github.com>
2025-05-06 00:08:21 +0900

    🚑 fix: Add matches inthe defineContentScript

--- entrypoints/content/index.tsx
+++ entrypoints/content/index.tsx
@@ -2,6 +2,7 @@ import { injectButton } from './inject_btn'
 import { injectFavoriteListComponent } from './inject_favorite_list'
 
 export default defineContentScript({
+  matches: ['https://chzzk.naver.com/*'],
   main (_) {
     injectButton() // .catch(console.error)
     injectFavoriteListComponent()
