f068b0b2723f921f2ed7769695de7ffac1ea7041
khk4912 <kosame@kosame.dev>
2025-05-10 15:49:31 +0900

    🏷️ feat: Add Document Picture-in-Picture interfaces and options

--- cheese-pip.d.ts
+++ cheese-pip.d.ts
@@ -3,6 +3,8 @@
 /// <reference types="navigation-api-types" />
 /// <reference types="../cheese-pip" />
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 interface HTMLVideoElement {
   captureStream: () => MediaStream
   mozCaptureStream: () => MediaStream
@@ -36,4 +38,54 @@ interface MediaRecorder {
   stopFlag?: boolean
   blobSize?: number
   videoBlob?: Blob[]
+}
+
+// Document Picture-in-Picture
+// Specification: https://wicg.github.io/document-picture-in-picture/
+// Repository: https://github.com/WICG/document-picture-in-picture
+
+declare let documentPictureInPicture: DocumentPictureInPicture
+
+declare class DocumentPictureInPicture extends EventTarget {
+  requestWindow (options?: DocumentPictureInPictureOptions): Promise<Window>
+  readonly window: Window
+  onenter: ((this: DocumentPictureInPicture, ev: DocumentPictureInPictureEvent) => any) | null
+  addEventListener<K extends keyof DocumentPictureInPictureEventMap>(type: K, listener: (this: DocumentPictureInPicture, ev: DocumentPictureInPictureEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void
+  addEventListener (type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void
+  removeEventListener<K extends keyof DocumentPictureInPictureEventMap>(type: K, listener: (this: DocumentPictureInPicture, ev: DocumentPictureInPictureEventMap[K]) => any, options?: boolean | EventListenerOptions): void
+  removeEventListener (type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void
+}
+
+interface DocumentPictureInPictureEventMap {
+  center: DocumentPictureInPictureEvent;
+}
+
+interface DocumentPictureInPictureOptions {
+  width?: number;
+  height?: number;
+  initialAspectRatio?: number;
+  lockAspectRatio?: boolean;
+  copyStyleSheets?: boolean;
+}
+
+declare class DocumentPictureInPictureEvent {
+  constructor (type: string, eventInitDict: DocumentPictureInPictureEventInit)
+  readonly window: Window
+}
+
+interface DocumentPictureInPictureEventInit {
+  window: Window;
+}
+
+interface Window {
+  documentPictureInPicture: DocumentPictureInPicture
+  documentPictureInPictureEnabled: boolean
+  documentPictureInPictureWindow: Window
+  documentPictureInPictureWindowId: string
+  documentPictureInPictureWindowState: string
+  documentPictureInPictureWindowWidth: number
+  documentPictureInPictureWindowHeight: number
+  documentPictureInPictureWindowAspectRatio: number
+  documentPictureInPictureWindowLockAspectRatio: boolean
+  documentPictureInPictureWindowCopyStyleSheets: boolean
 }
