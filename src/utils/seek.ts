39745819aa58505df12ecf6de14a8b0d53122a11
khk4912 <30457148+khk4912@users.noreply.github.com>
2025-06-09 15:15:38 +0900

    🩹 fix: Add buffer check in seekLeft and seekRight functions

--- src/utils/seek.ts
+++ src/utils/seek.ts
@@ -2,6 +2,8 @@ const SEEK_SECONDS = 5
 const FRAGMENT_DURATION = 1
 
 export function seekLeft (video: HTMLVideoElement): void {
+  if (video.buffered.length === 0) { return }
+
   if (video.currentTime - SEEK_SECONDS <= video.buffered.start(0)) {
     video.currentTime = video.buffered.start(0) + FRAGMENT_DURATION
   } else {
@@ -10,6 +12,8 @@ export function seekLeft (video: HTMLVideoElement): void {
 }
 
 export function seekRight (video: HTMLVideoElement): void {
+  if (video.buffered.length === 0) { return }
+
   if (video.currentTime + SEEK_SECONDS - FRAGMENT_DURATION >= video.buffered.end(video.buffered.length - 1)) {
     video.currentTime = video.buffered.end(video.buffered.length - 1) - FRAGMENT_DURATION
   } else {
